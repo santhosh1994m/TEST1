e7a7eae1aa0fa9e1713d2073f52017320a412e77
DKAnsh <www.dkansh.gov.in@gmail.com>
2018-06-20 12:18:26 +0530

    first commit

---
+++ src/main/java/org/dkansh/JenkinsWebAppApplication.java
@@ -0,0 +1,12 @@
+package org.dkansh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsWebAppApplication.class, args);
+	}
+}
